4a41768897cbb6526e3d1ffb342f954ee820b2f4
michael <michibinyamin@gmail.com>
2024-08-01 22:14:28 +0300

    fixes

--- hello/hello.c
+++ hello/hello.c
@@ -1,32 +1,25 @@
 /*
-* File: hello.c
-*/
+ * File: hello.c
+ */
 #include <stdlib.h>
 #include <string.h>
-//#include <stdio.h>
+#include <stdio.h>
+
 int main(int argc, char *argv[]) {
     char *string, *string_so_far;
-    int i, length; length = 0;
-
+    int i, length;     length = 0;
     for(i=0; i<argc; i++) {
         length += strlen(argv[i])+1;
         string = malloc(length+1);
-        /* * Copy the string built so far. */
-        if(string_so_far != (char *)0){
-            strcpy(string, string_so_far);
-        }
-        else{
-            *string = '\0';
-        } 
 
-        strcat(string, argv[i]);
-        
-        if(i < argc-1) {
-            strcat(string, " ");
-        }
-        string_so_far = string;
+        /*  * Copy the string built so far. */
+        if(string_so_far != (char *)0)  // Checks if string so far is not null
+            strcpy(string, string_so_far);
+        else *string = '\0';
+        strcat(string, argv[i]);    // Adds a null-terminator at the end
+        if(i < argc-1) strcat(string, " ");
+        string_so_far = string;     // Memory leak here! the allocated memory that was assigned to string has no pointer
     }
     printf("You entered: %s\n", string_so_far);
-    free(string);
     return (0);
 }
