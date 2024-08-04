ae93f7a65e49582e888d925ffea92f287c890f03
michael <michibinyamin@gmail.com>
2024-08-04 18:03:58 +0300

    fixes

--- PART B/main.cpp
+++ PART B/main.cpp
@@ -1,20 +1,55 @@
-// main.cpp
 #include <iostream>
-#include "Singleton.h"
-//#include "ScopeMutex.h"
+#include <memory>
 
-class MySingleton : public SingletonBase {
+// Forward declaration of ConcreteSingleton
+class ConcreteSingleton;
+
+class AbstractSingleton {
 public:
-    void someMethod() override {
-        std::cout << "Singleton method called!" << std::endl;
-    }
+    virtual ~AbstractSingleton() = default;
+
+    // Pure virtual method (abstract method)
+    virtual void doSomething() const = 0;
+
+    // Static method to access the singleton instance
+    static AbstractSingleton* getInstance();
+
+    // Delete copy constructor and assignment operator to prevent copying
+    AbstractSingleton(const AbstractSingleton&) = delete;
+    AbstractSingleton& operator=(const AbstractSingleton&) = delete;
+
+protected:
+    AbstractSingleton() = default; // Protected constructor for singleton pattern
+
+private:
+    static AbstractSingleton* instance;
 };
 
-int main() {
-    SingletonBase* singleton = MySingleton::getInstance();
+// Initialize static member
+AbstractSingleton* AbstractSingleton::instance = nullptr;
 
-    // Use the singleton
-    singleton->someMethod();
+// Definition of ConcreteSingleton class
+class ConcreteSingleton : public AbstractSingleton {
+public:
+    void doSomething() const override {
+        std::cout << "Doing something in ConcreteSingleton" << std::endl;
+    }
+
+    ConcreteSingleton() = default;
+    ~ConcreteSingleton() override = default;
+};
+
+// Implementation of getInstance method
+AbstractSingleton* AbstractSingleton::getInstance() {
+    if (!instance) {
+        instance = new ConcreteSingleton();
+    }
+    return instance;
+}
+
+int main() {
+    AbstractSingleton* singleton = AbstractSingleton::getInstance();
+    singleton->doSomething();
 
     return 0;
 }
